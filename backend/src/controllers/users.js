fc178cf3704f2485a735e84a65f2a61539be2335
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-27 00:54:55 -0300

    Criado uma verificação de campos obrigatorios e a logica da hash para senha

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,17 +1,28 @@
 const pool = require("../config/database")
+const bcrypt = require('bcrypt');
 
 const userRegister = async (req, res) => {
     const { name, cpf, email, phone, password } = req.body;
 
     try {
+        if (!name || !cpf || !email || !phone || !password) {
+            return res.status(400).json({ mensagem: `Todos os campos são obrigatórios` });
+        }
 
-        const newUser = await pool.query('INSERT INTO owners (name, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
-            [name, cpf, email, phone, password]
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const newUser = await pool.query('INSERT INTO owners (name, cpfHash, email, phone, passwordHash) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
+            [name, cpf, email, phone, passwordHash]
         )
+
 
         return res.status(201).json(newUser.rows[0])
     } catch (error) {
-        return res.status(500).json({ mensagem: `Erro interno do servidor ${error}` });
+        console.log(error.message);
+        console.log(name, cpf, email, phone, password);
+
+
+        return res.status(500).json({ mensagem: `Erro interno do servidor` });
 
     }
 }
