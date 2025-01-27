607bf4d434c7e196e8a687fb97951e59bbea1d8f
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-27 20:40:01 -0300

    Alterada a forma que e feita a criptografica para ser fixa e possivel de fazer compração direta

--- backend/src/config/encryption.js
+++ backend/src/config/encryption.js
@@ -1,27 +1,23 @@
 const crypto = require('crypto');
 
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
-const IV_LENGTH = 16;
+const IV = Buffer.alloc(16, 0); // IV fixo (menos seguro)
 
 function encrypt(text) {
-  const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), IV);
   let encrypted = cipher.update(text, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
-  return iv.toString('hex') + ':' + encrypted;
-};
+  return encrypted;
+}
 
 function decrypt(text) {
-  const textParts = text.split(':');
-  const iv = Buffer.from(textParts.shift(), 'hex');
-  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
-  let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), IV);
+  let decrypted = decipher.update(text, 'hex', 'utf-8');
   decrypted += decipher.final('utf-8');
   return decrypted;
-};
+}
 
 module.exports = {
   encrypt,
-  decrypt
+  decrypt,
 };
