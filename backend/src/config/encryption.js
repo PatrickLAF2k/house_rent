66bcc21f2c22ff6cd2de48b30244577852ecec59
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-27 01:21:51 -0300

    Criado o arquivo para fazer a criptografia do cpf e adicionado a logica para a mesma no cadastro do usuario

---
+++ backend/src/config/encryption.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text, 'utf-8', 'hex');
+  encrypted += cipher.final('hex');
+  return iv.toString('hex') + ':' + encrypted;
+};
+
+function decrypt(text) {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
+  decrypted += decipher.final('utf-8');
+  return decrypted;
+};
+
+module.exports = {
+  encrypt,
+  decrypt
+};
--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,5 +1,6 @@
 const pool = require("../config/database")
 const bcrypt = require('bcrypt');
+const { encrypt } = require("../config/encryption")
 
 const userRegister = async (req, res) => {
     const { name, cpf, email, phone, password } = req.body;
@@ -10,19 +11,16 @@ const userRegister = async (req, res) => {
         }
 
         const passwordHash = await bcrypt.hash(password, 10);
+        const encryptedCpf = encrypt(cpf);
 
-        const newUser = await pool.query('INSERT INTO owners (name, cpfHash, email, phone, passwordHash) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
-            [name, cpf, email, phone, passwordHash]
+        const newUser = await pool.query('INSERT INTO owners (name, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
+            [name, encryptedCpf, email, phone, passwordHash]
         )
 
+        return res.status(201).json(newUser.rows[0]);
 
-        return res.status(201).json(newUser.rows[0])
     } catch (error) {
-        console.log(error.message);
-        console.log(name, cpf, email, phone, password);
-
-
-        return res.status(500).json({ mensagem: `Erro interno do servidor` });
+        return res.status(500).json({ mensagem: `Erro interno do servidor (${error.message})` });
 
     }
 }
