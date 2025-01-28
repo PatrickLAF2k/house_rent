75dfe5eb23f9fd2a70815ea80c82ec9ea5eeb204
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-28 18:51:34 -0300

    Criado o arquivo para registrar o inquilino e atualizado a rota no app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const tokenVerify = require('./middlewares/authentication');
 const { userRegister } = require('./controllers/userRegister');
 const { userlogin } = require('./controllers/userLogin');
+const { tenantsRegister } = require('./controllers/tenantsRegister');
 const app = express();
 
 // Middleware
 app.use(express.json());
 
 // Rotas
-app.post('/register', userRegister );
-app.post('/login', userlogin );
+app.post('/register', userRegister);
+app.post('/login', userlogin);
 
 app.use(tokenVerify)
+app.post('/tenantsRegister', tenantsRegister);
 
 app.get('/user', (req, res) => {
     res.status(200).json({ mensagem: "Rota protegida" });
--- backend/src/controllers/tenantsRegister.js
+++ backend/src/controllers/tenantsRegister.js
@@ -1,29 +1,34 @@
 const pool = require("../config/database")
-const bcrypt = require('bcrypt');
 const { encrypt, decrypt } = require("../config/encryption")
 
-const userRegister = async (req, res) => {
-    const { name, cpf, email, phone } = req.body;
+const tenantsRegister = async (req, res) => {
+    const { name, cpf, email, phone, date_of_birth, rg, address, address_number, neighborhood, municipality, state, zip_code } = req.body;
+
 
     try {
-        if (!name || !cpf || !email || !phone) {
+        if (!name || !cpf || !email || !phone || !date_of_birth || !rg || !address || !address_number || !neighborhood || !municipality || !state || !zip_code) {
             return res.status(400).json({ mensagem: `Todos os campos são obrigatórios` });
         }
 
-        const existingEmail = await pool.query("SELECT * FROM owners WHERE email = $1", [email]);
+        const existingEmail = await pool.query("SELECT * FROM tenants WHERE email = $1", [email]);
         if (existingEmail.rows.length > 0) {
             return res.status(409).json({ mensagem: `Email já cadastrado` });
         }
 
         const encryptedCpf = encrypt(cpf);
-        const existingCpf = await pool.query("SELECT * FROM owners WHERE cpf = $1", [encryptedCpf]);
+        const existingCpf = await pool.query("SELECT * FROM tenants WHERE cpf = $1", [encryptedCpf]);
         if (existingCpf.rows.length > 0) {
             return res.status(409).json({ mensagem: `CPF já cadastrado` });
         }
 
-        const passwordHash = await bcrypt.hash(password, 10);
-        const newUser = await pool.query('INSERT INTO owners (name, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
-            [name, encryptedCpf, email, phone, passwordHash])
+        const encryptedRg = encrypt(rg);
+        const existingRg = await pool.query("SELECT * FROM tenants WHERE rg = $1", [encryptedRg]);
+        if (existingRg.rows.length > 0) {
+            return res.status(409).json({ mensagem: `RG já cadastrado` });
+        }
+
+        const newUser = await pool.query('INSERT INTO tenants (name, cpf, email, phone, date_of_birth, rg, address, address_number, neighborhood, municipality, state, zip_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING * ',
+            [name, encryptedCpf, email, phone, date_of_birth, encryptedRg, address, address_number, neighborhood, municipality, state, zip_code])
 
         return res.status(201).json(newUser.rows[0]);
 
@@ -33,5 +38,5 @@ const userRegister = async (req, res) => {
 }
 
 module.exports = {
-    userRegister
+    tenantsRegister
 }
