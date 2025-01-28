4f0e2a9b2cc24d8c0edfe81aa80cc1fbec69bf7b
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-28 18:50:41 -0300

    Atualizado o crud do banco de dados

--- backend/dumb.sql
+++ backend/dumb.sql
@@ -5,9 +5,9 @@ CREATE TABLE
     owners (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        cpf VARCHAR(70)  UNIQUE NOT NULL,
+        cpf VARCHAR(70) UNIQUE NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
-        phone VARCHAR(30)  NOT NULL,
+        phone VARCHAR(30) NOT NULL,
         password VARCHAR(255) NOT NULL
     );
 
@@ -16,9 +16,17 @@ CREATE TABLE
     tenants (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        cpf VARCHAR(70)  NOT NULL,
+        cpf VARCHAR(70) UNIQUE NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
-        phone VARCHAR(30)  NOT NULL
+        phone VARCHAR(30) NOT NULL,
+        date_of_birth DATE NOT NULL,
+        rg VARCHAR(70) UNIQUE NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        address_number VARCHAR(10) NOT NULL,
+        neighborhood VARCHAR(255) NOT NULL,
+        municipality VARCHAR(100) NOT NULL,
+        state VARCHAR(2) NOT NULL,
+        zip_code VARCHAR(8) NOT NULL
     );
 
 -- Tabela de imóveis
@@ -31,18 +39,18 @@ CREATE TABLE
         neighborhood VARCHAR(255),
         municipality VARCHAR(100),
         city VARCHAR(255) NOT NULL,
-        state VARCHAR(255) NOT NULL
+        state VARCHAR(2) NOT NULL
     );
 
 -- Tabela de contratos
 CREATE TABLE
     contract (
         id SERIAL PRIMARY KEY,
-        owner_id INT NOT NULL REFERENCES owners (id) ON DELETE CASCADE,
-        tenant_id INT NOT NULL REFERENCES tenants (id) ON DELETE CASCADE,
-        property_id INT NOT NULL REFERENCES properties (id) ON DELETE CASCADE,
+        owner_id INT NOT NULL REFERENCES owners (id),
+        tenant_id INT NOT NULL REFERENCES tenants (id),
+        property_id INT NOT NULL REFERENCES properties (id),
         start_date TIMESTAMP NOT NULL,
         end_date TIMESTAMP NOT NULL,
-        rent_value INTEGER NOT NULL,
-        payment_day INTEGER NOT NULL
+        rent_value INT NOT NULL,
+        payment_day INT NOT NULL
     );
