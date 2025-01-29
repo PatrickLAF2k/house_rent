bad08a45b49db9af3bf76e6c1921119f497f7f6a
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-01-29 00:54:26 -0300

    Criada funcao para verificar os campos e comentarios para facil entendimento do cod

--- backend/src/controllers/userRegister.js
+++ backend/src/controllers/userRegister.js
@@ -2,25 +2,39 @@ const pool = require("../config/database")
 const bcrypt = require('bcrypt');
 const { encrypt, decrypt } = require("../config/encryption")
 
+// Função para verificar campos obrigatórios
+const checkRequiredFields = (fields) => {
+    for (const field in fields) {
+        if (!fields[field]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 const userRegister = async (req, res) => {
     const { name, cpf, email, phone, password } = req.body;
 
     try {
-        if (!name || !cpf || !email || !phone || !password) {
+        // Verificação de campos obrigatórios
+        if (!checkRequiredFields({ name, cpf, email, phone, password })) {
             return res.status(400).json({ mensagem: `Todos os campos são obrigatórios` });
         }
 
+        // Verificação de email existente
         const existingEmail = await pool.query("SELECT * FROM owners WHERE email = $1", [email]);
         if (existingEmail.rows.length > 0) {
             return res.status(409).json({ mensagem: `Email já cadastrado` });
         }
 
+        // Verificação de CPF existente
         const encryptedCpf = encrypt(cpf);
         const existingCpf = await pool.query("SELECT * FROM owners WHERE cpf = $1", [encryptedCpf]);
         if (existingCpf.rows.length > 0) {
             return res.status(409).json({ mensagem: `CPF já cadastrado` });
         }
 
+        // Inserção de novo usuário
         const passwordHash = await bcrypt.hash(password, 10);
         const newUser = await pool.query('INSERT INTO owners (name, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
             [name, encryptedCpf, email, phone, passwordHash])
