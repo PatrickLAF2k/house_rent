7315d644fa3c14e6959025ab6385eb35a5e62be5
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-02-05 19:33:55 -0300

    Melhorada a autenticação de token JWT com verificações adicionais e tratamento de erros

--- backend/src/middlewares/authentication.js
+++ backend/src/middlewares/authentication.js
@@ -1,28 +1,39 @@
 const jwt = require('jsonwebtoken');
-const pool = require("../config/database")
+const pool = require("../config/database");
+
+// Obtém a chave secreta do JWT a partir das variáveis de ambiente
 const JWT_SECRET = process.env.JWT_SECRET;
 
+
 const authenticateToken = async (req, res, next) => {
+    // Obtém o cabeçalho de autorização da requisição
     const { authorization } = req.headers;
+
+    // Verifica se o cabeçalho de autorização está presente
     if (!authorization) {
         return res.status(401).json({ mensagem: "Acesso negado. Token não fornecido." });
     }
 
+    // Extrai o token do cabeçalho de autorização
     const token = authorization.split(' ')[1];
 
     try {
+        // Verifica e decodifica o token JWT
         const { id } = jwt.verify(token, JWT_SECRET);
+
+        // Consulta o banco de dados para verificar se o usuário existe
         const result = await pool.query('SELECT * FROM owners WHERE id = $1', [id]);
         const user = result.rows[0];
-        console.log(user);
-        
 
+        // Verifica se o usuário foi encontrado
         if (!user) {
             return res.status(403).json({ mensagem: "Token inválido." });
         }
+
+        // Adiciona o usuário ao objeto de requisição para uso posterior
         req.user = user;
 
-        next()
+        next();
 
     } catch (error) {
         return res.status(500).json({ mensagem: `Erro interno do servidor (${error})` });
