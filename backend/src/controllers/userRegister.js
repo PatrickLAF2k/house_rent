988505dbbce3ca941e94fa19096b055c0ed290c8
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-02-06 17:29:47 -0300

    Adiciona novos campos ao registro de usuários e melhora a inserção no banco de dados

--- backend/src/controllers/userRegister.js
+++ backend/src/controllers/userRegister.js
@@ -13,11 +13,11 @@ const checkRequiredFields = (fields) => {
 }
 
 const userRegister = async (req, res) => {
-    const { name, cpf, email, phone, password } = req.body;
+    const { name, nationality, marital_status, occupation, date_of_birth, rg, issuing_authority, cpf, email, phone, password } = req.body;
 
     try {
         // Verificação de campos obrigatórios
-        if (!checkRequiredFields({ name, cpf, email, phone, password })) {
+        if (!checkRequiredFields({ name, nationality, marital_status, occupation, date_of_birth, rg, issuing_authority, cpf, email, phone, password })) {
             return res.status(400).json({ mensagem: `Todos os campos são obrigatórios` });
         }
 
@@ -35,9 +35,10 @@ const userRegister = async (req, res) => {
         }
 
         // Inserção de novo usuário
+        const encryptedRg = encrypt(rg);
         const passwordHash = await bcrypt.hash(password, 10);
-        const newUser = await pool.query('INSERT INTO owners (name, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5) RETURNING * ',
-            [name, encryptedCpf, email, phone, passwordHash])
+        const newUser = await pool.query('INSERT INTO owners (name, nationality, marital_status, occupation, date_of_birth, rg, issuing_authority, cpf, email, phone, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id,name, email  ',
+            [name, nationality, marital_status, occupation, date_of_birth, encryptedRg, issuing_authority, encryptedCpf, email, phone, passwordHash])
 
         return res.status(201).json(newUser.rows[0]);
 
