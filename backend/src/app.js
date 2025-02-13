34201bfceef31193727e8dbe3b57e7c0c19b30bd
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-02-12 21:43:47 -0300

    Adiciona middleware de validação para login de usuário

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const authenticateToken = require('./middlewares/authentication');
+
+//Rotas
 const { userRegister } = require('./controllers/userRegister');
 const { userlogin } = require('./controllers/userLogin');
 const { tenantsRegister } = require('./controllers/tenantsRegister');
 const { propertiesRegister } = require('./controllers/propertiesRegister');
 const { contractRegister } = require('./controllers/contractRegister');
 const validateUser = require('./middlewares/validateUser');
+const validateLogin = require('./middlewares/validateLogin');
+
 const app = express();
 
-// Middleware
 app.use(express.json());
 
 // Rotas
 app.post('/register', validateUser, userRegister);
-app.post('/login', userlogin);
+app.post('/login', validateLogin, userlogin);
 
 app.use(authenticateToken)
+
 app.post('/tenants/register', tenantsRegister);
 app.post('/properties/register', propertiesRegister);
 app.post('/contract/register', contractRegister);
